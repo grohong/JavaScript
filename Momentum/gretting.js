84226f89d2ff9ab5e199efdeaabaf0cdc0948454
grohong <grohong76@gmail.com>
2018-11-18 16:53:10 +0900

    set gretting

--- Momentum/gretting.js
+++ Momentum/gretting.js
@@ -1,11 +1,28 @@
 const form = document.querySelector(".js-form"),
-    intpu = form.querySelector("input");
+    input = form.querySelector("input");
     greeting = document.querySelector(".js-greetings")
 
 const USER_LS = "currnetUser",
-    SHOING_CN = "showing";
+    SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
@@ -13,7 +30,7 @@ function paintGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS)
     if(currentUser === null) {
-        // she is not
+        askForName()
     } else {
         paintGreeting(currentUser)
     }
