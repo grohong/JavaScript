5b04dcc57095e01f40f823a4a3a966eb528784a3
grohong <grohong76@gmail.com>
2018-11-18 23:07:13 +0900

    set weather

--- Momentum/weather.js
+++ Momentum/weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "";
 const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metrics`
+    )
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function handleGeoSuccess(position) {
     const latitude = position.coords.latitude;
@@ -8,6 +25,7 @@ function handleGeoSuccess(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function saveCoords(coordsObj) {
@@ -23,11 +41,12 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(41.40338, 2.17403);
     }
 }
 
